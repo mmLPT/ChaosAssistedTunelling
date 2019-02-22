0ae61f1e7f89e43e034ea6697953308cb3cd4b54
LPTMartinezMaxime <martinez@irsamc.ups-tlse.fr>
2019-02-22 11:53:25 +0100

    corrections to make possible //

--- utils/classical/stroboscopic.py
+++ utils/classical/stroboscopic.py
@@ -9,19 +9,20 @@ class StrobosopicPhaseSpace:
 	# This class can be used to generate a stroposcopic phase space of a 
 	# given time periodic system. It requires time propagator, to include 
 	# both discrete and continous time map.
-	def __init__(self,nperiod,ny0,timepropagator,xmax=2*np.pi,pmax=2*np.pi):
+	def __init__(self,nperiod,ny02,timepropagator,xmax=2*np.pi,pmax=2*np.pi):
 		self.nperiod=nperiod
-		self.ny0=ny0
-		self.y0=np.zeros(2)
+		self.ny0=int(np.sqrt(ny0))
+		self.random=False
 		self.xmax=xmax
 		self.pmax=pmax
+		self.dx=self.xmax/self.ny0
+		self.dy=self.ysmax/self.ny0
 		self.timepropagator=timepropagator
 	
-	def getTrajectory(self,i,j=0):
+	def getTrajectory(self,i):
 		# this fonciton computes strobsoocpic trajectory over nperiod 
 		# for a given initial state y0
-		self.sety0(i,j)
-		y=self.y0
+		y=self.generatey0(i)
 		xs=np.zeros(self.nperiod)
 		ps=np.zeros(self.nperiod)
 		for k in range(0,self.nperiod):
@@ -34,18 +35,13 @@ class StrobosopicPhaseSpace:
 		
 		return xs, ps
 	
-	def save(self,wdir="",double=False):
-		if double:
-			for i in range(0,self.ny0):
-				for j in range(0,self.ny0):
-					xs,ps = self.getTrajectory(i,j)
-					np.savez(wdir+strint(i*self.ny0+j),"w", x=xs, p=ps)
-		else:
-			for i in range(0,self.ny0):
+	def save(self,wdir=""):
+		for i in range(0,self.ny0):
+			for j in range(0,self.ny0):
 				xs,ps = self.getTrajectory(i)
-				np.savez(wdir+strint(i),"w", x=xs, p=ps)
+				np.savez(wdir+strint(i*self.ny0+j),"w", x=xs, p=ps)
 			
-	def npz2plt(self, wdir="",double=False):
+	def npz2plt(self, wdir=""):
 		setLatex()
 		# Read .npz file and print it
 		ax = plt.axes()
@@ -53,22 +49,20 @@ class StrobosopicPhaseSpace:
 		ax.set_ylim(-self.pmax/2.0,self.pmax/2.0)
 		#ax.set_ylim(-1.0,1.0)
 		ax.set_aspect('equal')
-		if double:
-			for i in range(0,self.ny0):
-				for j in range(0,self.ny0):
-					data=np.load(wdir+strint(i*self.ny0+j)+".npz")
-					x=data["x"]
-					p=data["p"]
-					plt.scatter(x,p,s=0.1**2,c="black")
-			plt.show()
-		else:
-			for i in range(0,self.ny0):
-				data=np.load(wdir+strint(i)+".npz")
+		for i in range(0,self.ny0):
+			for j in range(0,self.ny0):
+				data=np.load(wdir+strint(i*self.ny0+j)+".npz")
 				x=data["x"]
 				p=data["p"]
 				plt.scatter(x,p,s=0.1**2,c="black")
-			plt.show()
+		plt.show()
+
 		#saveLatex("classicalPS")
 		
-	def sety0(self,i,j):
-		self.y0=np.array([rd.randint(0,101)/100.0*self.xmax-self.xmax/2.0,rd.randint(0,101)/100.0*self.pmax-self.pmax/2.0])
+	def generatey0(self,i):
+		if random==False:
+			ix=int(i/ny0)
+			ip=i%ny0
+			return np.array([-self.xmax/2.0+ix*self.dx,-self.pmax/2.0+ip*self.dp])
+		else :
+			return np.array([rd.randint(0,101)/100.0*self.xmax-self.xmax/2.0,rd.randint(0,101)/100.0*self.pmax-self.pmax/2.0])
